8f7769f34980a173f43d50102387511f30461592
radsatjob <radhikatk@gmail.com>
2025-04-14 20:15:47 +0530

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Portfolio',
-  tagline: 'Radhika is coolest and hittest :)',
+  tagline: 'Radhika is coolest and hottest :)',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
